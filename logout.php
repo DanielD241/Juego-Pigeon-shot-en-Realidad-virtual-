1b098e6942c14fb86a633feb1e863646570ca6ad
DanielD241 <157421136+DanielD241@users.noreply.github.com>
2026-05-21 15:06:11 -0500

    Actualizar logout.php

--- logout.php
+++ logout.php
@@ -2,12 +2,43 @@
 
 session_start();
 
-// BORRAR TODA LA SESIÓN
-session_unset();
+/* =========================
+   ELIMINAR VARIABLES
+   ========================= */
+
+$_SESSION = [];
+
+/* =========================
+   ELIMINAR COOKIE DE SESIÓN
+   ========================= */
+
+if (ini_get("session.use_cookies")) {
+
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+/* =========================
+   DESTRUIR SESIÓN
+   ========================= */
+
 session_destroy();
 
-// REDIRIGIR AL LOGIN
+/* =========================
+   REDIRIGIR
+   ========================= */
+
 header("Location: login.html");
+
 exit();
 
 ?>
